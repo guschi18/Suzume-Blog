7bc6dee80235c73d28641f15fabc890c92140b56
guschi18 <philipp.guschl@gmx.de>
2023-05-31 14:24:25 +0200

    Website yourDetails

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -2,7 +2,7 @@
 export const contractAddress = "0xD749BcC40974C0E9513E41dfb747ba1D5fB51D7f";
 
 // Replace this with your domain name.
-export const domainName = "example.com";
+export const domainName = "https://suzume-blog-1at7jae0q-guschi18.vercel.app/";
 
 // Minimum number of tokens required to access
 export const minimumBalance = 1;
