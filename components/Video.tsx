6d4cd9caf5966c892d3c208c7ca0b051c99776aa
guschi18 <philipp.guschl@gmx.de>
2023-07-12 21:24:38 +0200

    Videos

--- components/Video.tsx
+++ components/Video.tsx
@@ -4,11 +4,11 @@ const Home: React.FC = () => {
   return (
     <div className=" grid grid-cols-1 md:grid-cols-2 pt-2 gap-2 ">
       <div className='flex mt-9 justify-center'>
-    <iframe id="ytplayer" type="text/html" width="420" height="238"
+    <iframe id="ytplayer"  width="420" height="238"
   src="http://www.youtube.com/embed/E0PHnSgBCZI?autoplay=1&origin=http://example.com"
-  frameborder="0" allowfullscreen/></div>
+   /></div>
     <div className='flex justify-center'>
-    <iframe width="420" height="315" src="https://player.vimeo.com/video/790485662"  frameborder="0" allowfullscreen></iframe>
+    <iframe width="420" height="315" src="https://player.vimeo.com/video/790485662"></iframe>
     </div>
     </div>
    )
