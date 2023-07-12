bcf5ca162f869b365d9db9d29fd783e26d391618
guschi18 <philipp.guschl@gmx.de>
2023-07-12 21:54:07 +0200

    VideoPerfekt

--- components/Video.tsx
+++ components/Video.tsx
@@ -4,8 +4,8 @@ const Home: React.FC = () => {
   return (
     <div className=" grid grid-cols-1 md:grid-cols-2 pt-2 gap-2 ">
       <div className='flex mt-9 justify-center'>
-    <iframe  width="420" height="238"
-  src="http://www.youtube.com/embed/E0PHnSgBCZI"
+    <iframe id="jt"  width="420" height="238"
+  src="https://www.youtube.com/embed/E0PHnSgBCZI"
    /></div>
     <div className='flex justify-center'>
     <iframe width="420" height="315" src="https://player.vimeo.com/video/790485662"></iframe>
