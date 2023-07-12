d5cf02d389afbd72b5c6e9b6304c27b2da4ca7e8
guschi18 <philipp.guschl@gmx.de>
2023-07-12 21:42:01 +0200

    Youtube

--- components/Video.tsx
+++ components/Video.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
     <div className=" grid grid-cols-1 md:grid-cols-2 pt-2 gap-2 ">
       <div className='flex mt-9 justify-center'>
     <iframe  width="420" height="238"
-  src="https://player.vimeo.com/video/790485662"
+  src="http://www.youtube.com/embed/E0PHnSgBCZI"
    /></div>
     <div className='flex justify-center'>
     <iframe width="420" height="315" src="https://player.vimeo.com/video/790485662"></iframe>
