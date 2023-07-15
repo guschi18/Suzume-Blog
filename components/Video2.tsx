603823140f46bc499c775409c8c06c3c8f28d785
guschi18 <philipp.guschl@gmx.de>
2023-07-15 11:23:47 +0200

    VideoGröße

--- components/Video2.tsx
+++ components/Video2.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div className="flex justify-center ">
     <div className='flex justify-center'>
-    <iframe width="450" height="315" src="https://www.youtube.com/embed/LAhwyEGm8mM"></iframe>
+    <iframe width="420" height="315" src="https://www.youtube.com/embed/LAhwyEGm8mM"></iframe>
     </div>
     </div>
     )
