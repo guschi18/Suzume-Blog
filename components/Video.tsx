d6d9e774405f9c8e49e52353188077e291dadd80
guschi18 <philipp.guschl@gmx.de>
2023-07-21 16:51:32 +0200

    TestVideoIphone

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,11 +1,13 @@
 function Video(){
 return (
 <div className="flex justify-center mt-1 pt-4 ">
-  <video 
-  className="flex max-h-96 rounded-md playsinline"
-  src="https://firebasestorage.googleapis.com/v0/b/suzume-82712.appspot.com/o/Shita-Kiri-Suzume_%20An%20Origin%20Story.mp4?alt=media&token=54811c25-9717-40a4-b4f9-8e26c9ccdfbb"
-  controls
-  ></video>
+<video 
+                className="flex max-h-96 rounded-md playsinline"
+                controls
+            >
+                <source src="https://firebasestorage.googleapis.com/v0/b/suzume-82712.appspot.com/o/Shita-Kiri-Suzume_%20An%20Origin%20Story.mp4?alt=media&token=54811c25-9717-40a4-b4f9-8e26c9ccdfbb" type="video/mp4" />
+              
+            </video>
 </div>
 )
 }
