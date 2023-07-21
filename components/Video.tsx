0e6a1e3dd29934a949469ccd218757b988790944
guschi18 <philipp.guschl@gmx.de>
2023-07-21 17:03:52 +0200

    VideoBackground

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,6 +1,6 @@
 function Video(){
 return (
-<div className="flex justify-center mt-1 pt-4 ">
+<div className="flex justify-center mt-1 pt-4 bg-[url('https://firebasestorage.googleapis.com/v0/b/suzume-82712.appspot.com/o/Shita-Kiri-Suzume_%20An%20Origin%20Story.mp4?alt=media&token=54811c25-9717-40a4-b4f9-8e26c9ccdfbb')] ">
 <video 
                 className="flex max-h-96 rounded-md playsinline"
                 controls
