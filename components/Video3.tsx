0c57c5b2b206a29454fcc992f74ffd53f722d28a
guschi18 <philipp.guschl@gmx.de>
2023-07-22 22:03:41 +0200

    contract

--- components/Video3.tsx
+++ components/Video3.tsx
@@ -1,5 +1,6 @@
   function Video(){
     return (
+      <div>
     <div className="flex justify-center mt-1 pt-4] ">
     <video 
                     className="flex max-h-96 rounded-md playsinline"
@@ -9,6 +10,8 @@
                     <source src="https://firebasestorage.googleapis.com/v0/b/suzume-82712.appspot.com/o/video_2023-07-13_10-49-22.mp4?alt=media&token=578bdcdb-f362-49da-8dce-34a552dc1e2e" type="video/mp4" />
                   
                 </video>
+    </div>
+    <div className="flex justify-center font-serif text-lg mt-4 lg:text-xl">Token Contract: 0x0B452278223D3954F4AC050949D7998e373e7E43</div>
     </div>
     )
     }
