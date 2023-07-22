1c7421d1b73fe09f7f249ca464ee20e1444591a0
guschi18 <philipp.guschl@gmx.de>
2023-07-22 20:46:24 +0200

    Änderungen

--- components/Banner3.tsx
+++ components/Banner3.tsx
@@ -4,7 +4,7 @@ function Banner3() {
   return (
     <div>
         <hr className="my-6 border-black sm:mx-auto lg:my-8" />
-        <div className='flex justify-center mt-6 pt-8 underline text-5xl font-serif text-amber-950 '>
+        <div className='flex justify-center mt-6 pt-8 underline text-5xl font-serif '>
             <h1>Community Blog</h1>
         </div>
 
