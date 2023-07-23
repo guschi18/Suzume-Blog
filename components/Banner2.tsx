7513a2bb26f202a1e1d0cd71a4179cf7b2085174
guschi18 <philipp.guschl@gmx.de>
2023-07-23 15:41:27 +0200

    kp

--- components/Banner2.tsx
+++ components/Banner2.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Banner2() {
   return (
     <div >
-        <div className='flex justify-center mt-6 pt-8 underline text-5xl font-serif text-amber-950 '>
+        <div className='flex justify-center mt-6 pt-8 underline text-4xl lg:text-5xl font-serif text-amber-950 '>
             <h1>Shita-kiri Suzume</h1>
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ module.exports =({
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio', "daisyui"),
+    require('@tailwindcss/aspect-ratio', "daisyui", "'flowbite/plugin'"),
   ],
 });
